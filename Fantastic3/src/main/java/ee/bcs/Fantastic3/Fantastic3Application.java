31c45f85deb268dc1d962cc46fb688f25bdb2ccb
Wolfigg <mmremmet@gmail.com>
2022-02-14 10:34:10 +0200

    v1

---
+++ Fantastic3/src/main/java/ee/bcs/Fantastic3/Fantastic3Application.java
@@ -0,0 +1,13 @@
+package ee.bcs.Fantastic3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fantastic3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fantastic3Application.class, args);
+	}
+
+}
